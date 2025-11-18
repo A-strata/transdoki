7903e8869025f986caa1de23c2dbab38002f9060
Art <a.astakhin@gmail.com>
2025-11-18 21:22:26 +0300

    Littlt thing

--- persons/forms.py
+++ persons/forms.py
@@ -7,3 +7,13 @@ class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         exclude = ['created_by', 'created_at', 'updated_at']
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        for field in ['name', 'surname', 'patronymic']:
+            value = cleaned_data.get(field)
+            if value:
+                cleaned_data[field] = value.capitalize()
+
+        return cleaned_data
--- vehicles/forms.py
+++ vehicles/forms.py
@@ -15,11 +15,17 @@ class VehicleForm(forms.ModelForm):
         grn = cleaned_data.get('grn')
         vehicle_type = cleaned_data.get('vehicle_type')
 
+        if grn:
+            cleaned_data['grn'] = grn.upper()
+            grn = grn.upper()
+
         if grn and vehicle_type:
             try:
                 validate_grn_by_type(grn, vehicle_type)
             except ValidationError as e:
                 # Явно привязываем ошибку к полю grn
                 self.add_error('grn', e)
+            else:
+                cleaned_data['grn'] = grn.upper()
 
         return cleaned_data
