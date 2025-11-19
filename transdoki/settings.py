4d9a73078eb4cc1a9f1ab55f4b40689e17614cae
Art <a.astakhin@gmail.com>
2025-11-19 10:15:02 +0300

    Add static_root7

--- transdoki/settings.py
+++ transdoki/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles' # dgg
+STATIC_ROOT = BASE_DIR / 'staticfiles' # dg
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
