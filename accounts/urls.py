a5d3d39f9be719568092196dbb6ae8726c9c869f
Name Surname <a-strata@yandex.ru>
2026-03-22 13:26:31 +0300

    accounts: добавить read-only кабинет аккаунта

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import AccountCabinetView, RegisterView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("cabinet/", AccountCabinetView.as_view(), name="cabinet"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
+
+from transdoki.tenancy import get_request_account
 
 from .forms import AccountRegistrationForm
 
@@ -17,3 +20,21 @@ class RegisterView(FormView):
             "Регистрация завершена. Теперь войдите в систему.",
         )
         return super().form_valid(form)
+
+
+class AccountCabinetView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/cabinet.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        account = get_request_account(self.request)
+        profile = self.request.user.profile
+
+        context.update(
+            {
+                "account": account,
+                "profile": profile,
+            }
+        )
+        return context
