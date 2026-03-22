9f88c0a7ed0adcb7b2a684da6d5fc6dfda880f16
Name Surname <a-strata@yandex.ru>
2026-03-22 12:59:06 +0300

    просто formatter в accounts что-то подправил

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,39 +1,40 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
 from .models import UserProfile, UserSession
 
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
-    verbose_name_plural = 'Бизнес-профиль'
+    verbose_name_plural = "Бизнес-профиль"
 
 
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
     list_display = (
-        'username',
-        'email',
-        'get_max_sessions',
-        'get_max_companies',
-        'is_staff')
+        "username",
+        "email",
+        "get_max_sessions",
+        "get_max_companies",
+        "is_staff",
+    )
 
-    @admin.display(description='Лимит сессий')
+    @admin.display(description="Лимит сессий")
     def get_max_sessions(self, obj):
         return obj.profile.max_sessions
 
-    @admin.display(description='Лимит компаний')
+    @admin.display(description="Лимит компаний")
     def get_max_companies(self, obj):
         return obj.profile.max_own_companies
 
 
 @admin.register(UserSession)
 class UserSessionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'session_key', 'last_activity', 'is_active')
-    list_filter = ('is_active', 'user')
-    search_fields = ('session_key', 'user__username')
+    list_display = ("user", "session_key", "last_activity", "is_active")
+    list_filter = ("is_active", "user")
+    search_fields = ("session_key", "user__username")
 
 
 admin.site.unregister(User)
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 
 from .views import RegisterView
 
-# from . import views
-
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
